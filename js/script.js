468382d8d03c3f9eb8639f7b3c55bdb3d6245db1
Roman Naumov <romas.naumov@gmail.com>
2016-11-23 03:05:11 +0500

    Добавлена проверка на наличие элементов в документе.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //-----------------------------------------------------
-// Объявдение переменных
+// Объявление переменных
 //-----------------------------------------------------
+
 var button = document.querySelector(".feedback");
 var formModal = document.querySelector(".modal-content");
 var layer = document.querySelector(".modal-overlay");
@@ -25,84 +26,87 @@ var subsEmail = document.querySelector("#subs-email");
 var subsEmailHelp = document.querySelector("label[for=subs-email]");
       
       
-button.addEventListener("click", function(event) {
-  event.preventDefault();
-  formModal.classList.add("modal-content-show");
-  layer.classList.add("modal-overlay-show");
-  
-  if (storageUserName) {
-    userName.value = storageUserName;
-    if (storageUserEmail) {
-      userEmail.value = storageUserEmail;
-    } else {
-      userEmail.focus();
-    }
-  } else {
-    userName.focus();
-  }
-});
-
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  formModal.classList.remove("modal-content-show");
-  formModal.classList.remove("modal-content-error");
-  layer.classList.remove("modal-overlay-show");
-});
-
-form.addEventListener("submit", function(event) {
-  if (!userEmail.value) {
+if (button) {
+  button.addEventListener("click", function(event) {
     event.preventDefault();
-    //console.log("Нужно ввести Email");
+    formModal.classList.add("modal-content-show");
+    layer.classList.add("modal-overlay-show");
+  
+    if (storageUserName) {
+      userName.value = storageUserName;
+      if (storageUserEmail) {
+        userEmail.value = storageUserEmail;
+      } else {
+        userEmail.focus();
+      }
+    } else {
+      userName.focus();
+    }
+  });
+}
+
+if (formModal) {
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    formModal.classList.remove("modal-content-show");
     formModal.classList.remove("modal-content-error");
-    formModal.offsetWidth = formModal.offsetWidth;
-    formModal.classList.add("modal-content-error");
-  } else {
-    localStorage.setItem("userEmail", userEmail.value);
-  }
-  if (userName.value) {
-    localStorage.setItem("userName", userName.value);
-  }
-});
+    layer.classList.remove("modal-overlay-show");
+  });
 
-window.addEventListener("keydown", function(event) {
-  if (event.keyCode === 27) {
-    if (formModal.classList.remove("modal-content-show")) {
-      formModal.classList.remove("modal-content-show");
+  form.addEventListener("submit", function(event) {
+    if (!userEmail.value) {
+      event.preventDefault();
+      formModal.classList.remove("modal-content-error");
+      formModal.offsetWidth = formModal.offsetWidth;
+      formModal.classList.add("modal-content-error");
+    } else {
+      localStorage.setItem("userEmail", userEmail.value);
     }
-    if (layer.classList.remove("modal-overlay-show")) {
-      layer.classList.remove("modal-overlay-show");
+    if (userName.value) {
+      localStorage.setItem("userName", userName.value);
     }
-  }
-  formModal.classList.remove("modal-content-error");
-});
+  });
 
-userName.addEventListener("focus", function(event) {
-  userNameHelp.classList.add("ladel-visibility");
-  userNameHelp.classList.add("text-blue");
-});
+  window.addEventListener("keydown", function(event) {
+    if (event.keyCode === 27) {
+      if (formModal.classList.remove("modal-content-show")) {
+        formModal.classList.remove("modal-content-show");
+      }
+      if (layer.classList.remove("modal-overlay-show")) {
+        layer.classList.remove("modal-overlay-show");
+      }
+    }
+    formModal.classList.remove("modal-content-error");
+  });
 
-userName.addEventListener("blur", function(event) {
-  if (userName.value) {
-    userNameHelp.classList.remove("text-blue");
-  } else {
-    userNameHelp.classList.remove("ladel-visibility");
-    userNameHelp.classList.remove("text-blue");
-  }
-});
-
-userEmail.addEventListener("focus", function(event) {
-  userEmailHelp.classList.add("ladel-visibility");
-  userEmailHelp.classList.add("text-blue");
-});
-
-userEmail.addEventListener("blur", function(event) {
-  if (userEmail.value) {
-    userEmailHelp.classList.remove("text-blue");
-  } else {
-    userEmailHelp.classList.remove("ladel-visibility");
-    userEmailHelp.classList.remove("text-blue");
-  }
-});
+  userName.addEventListener("focus", function(event) {
+    userNameHelp.classList.add("ladel-visibility");
+    userNameHelp.classList.add("text-blue");
+  });
+  
+  userName.addEventListener("blur", function(event) {
+    if (userName.value) {
+      userNameHelp.classList.remove("text-blue");
+    } else {
+      userNameHelp.classList.remove("ladel-visibility");
+      userNameHelp.classList.remove("text-blue");
+    }
+  });
+  
+  userEmail.addEventListener("focus", function(event) {
+    userEmailHelp.classList.add("ladel-visibility");
+    userEmailHelp.classList.add("text-blue");
+  });
+  
+  userEmail.addEventListener("blur", function(event) {
+    if (userEmail.value) {
+      userEmailHelp.classList.remove("text-blue");
+    } else {
+      userEmailHelp.classList.remove("ladel-visibility");
+      userEmailHelp.classList.remove("text-blue");
+    }
+  });
+}
 
 entryEmail.addEventListener("focus", function(event) {
   entryEmailHelp.classList.add("ladel-visibility");
@@ -132,16 +136,18 @@ entryPassword.addEventListener("blur", function(event) {
   }
 });
 
-subsEmail.addEventListener("focus", function(event) {
-  subsEmailHelp.classList.add("ladel-visibility");
-  subsEmailHelp.classList.add("text-blue");
-});
-
-subsEmail.addEventListener("blur", function(event) {
-  if (subsEmail.value) {
-    subsEmailHelp.classList.remove("text-blue");
-  } else {
-    subsEmailHelp.classList.remove("ladel-visibility");
-    subsEmailHelp.classList.remove("text-blue");
-  }
- });
+if (subsEmail) {
+  subsEmail.addEventListener("focus", function(event) {
+    subsEmailHelp.classList.add("ladel-visibility");
+    subsEmailHelp.classList.add("text-blue");
+  });
+  
+  subsEmail.addEventListener("blur", function(event) {
+    if (subsEmail.value) {
+      subsEmailHelp.classList.remove("text-blue");
+    } else {
+      subsEmailHelp.classList.remove("ladel-visibility");
+      subsEmailHelp.classList.remove("text-blue");
+    }
+  });
+}
